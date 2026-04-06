565d45eb576aea408c4013a45a41233f4258abcf
Lakshiya-CS <lsenavaraiyanlavanya@mail.weber.edu>
2026-04-06 12:17:02 -0600

    done with ct16

--- src/BinarySearchTree.cpp
+++ src/BinarySearchTree.cpp
@@ -145,6 +145,10 @@ Node* BinarySearchTree::find_min_(Node* node) const {
     // TODO: Implement find_min_
     //   - Walk left until there is no left child
     //   - Return that node (it holds the minimum value)
+    while (node->left)
+    {
+        node = node->left;
+    }
     return nullptr;
 }
 
@@ -184,19 +188,53 @@ Node* BinarySearchTree::remove_(Node* node, int value, bool& removed) {
     // TODO: Implement remove_ (recursive, three cases)
     //
     //   Step 0: Base case — if node is null, return nullptr
-    //
+    if (!node) return nullptr;
     //   Step 1: if value < node->data, recurse left
+    if (value < node->data)
+    {
+        node->left = remove_(node->left, value, removed);
+    }
     //   Step 2: if value > node->data, recurse right
-    //
+    if (value > node->data)
+    {
+        node->right = remove_(node->right, value, removed);
+    }
     //   Step 3: value == node->data — FOUND IT, handle removal
-    //     - set removed = true
-    //     - Case 1 (leaf): delete node, return nullptr
-    //     - Case 2 (one child): delete node, return the child
-    //     - Case 3 (two children):
+    else
+    {
+        //     - set removed = true
+        removed = true;
+        //     - Case 1 (leaf): delete node, return nullptr
+        if (!node->left && !node->right)
+        {
+            delete node;
+            return nullptr;
+        }
+        //     - Case 2 (one child): delete node, return the child
+        if (!node->left)
+        {
+            Node* child = node->right;
+            delete node;
+            return child;
+        }
+        if (!node->right)
+        {
+            Node* child = node->left;
+            delete node;
+            return child;
+        }
+       // - Case 3 (two children):
+        Node* successor=find_min_(node->right);
+        node->data = successor->data;
+        node->right = remove_(node->right, successor->data, removed);
+    }
+
+
+
+    //
     //         a. find the in-order successor: find_min_(node->right)
     //         b. copy successor's data into node->data
     //         c. recursively remove the successor from the right subtree
     //
     //   Return node at the end
-    return nullptr;
 }
